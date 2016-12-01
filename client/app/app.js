bb14f05f7fbaacc76b04f2490ac13ccc2dedd6e5
zarinanorton <norton.zarina@gmail.com>
2016-12-01 13:55:15 -0500

    failbook

--- client/app/app.js
+++ client/app/app.js
@@ -6,6 +6,7 @@ import LeftSideBar from './components/leftsidebar';
 import RightSideBar from './components/rightsidebar';
 import ChatPopup from './components/chatpopup';
 import FeedItem from './components/feeditem';
+import ErrorBanner from './components/errorbanner';
 import {hideElement} from './util';
 import {searchForFeedItems, deleteFeedItem} from './server';
 import { IndexRoute, Router, Route, hashHistory } from 'react-router'
@@ -47,7 +48,7 @@ class SearchResultsPage extends React.Component {
     }
     return searchTerm;
   }
-  
+
   render() {
     var searchTerm = this.getSearchTerm();
     // By using the searchTerm as the key, React will create a new
@@ -67,13 +68,13 @@ class SearchResults extends React.Component {
       results: []
     };
   }
-  
+
   deleteFeedItem(id) {
     deleteFeedItem(id, () => {
       this.refresh();
     });
   }
-  
+
   refresh() {
     var searchTerm = this.props.searchTerm;
     if (searchTerm !== "") {
@@ -90,11 +91,11 @@ class SearchResults extends React.Component {
       });
     }
   }
-  
+
   componentDidMount() {
     this.refresh();
   }
-  
+
   render() {
     return (
       <div>
@@ -137,6 +138,11 @@ class App extends React.Component {
       <div>
         <NavBar searchTerm={searchTerm} />
         <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <ErrorBanner />
+            </div>
+          </div>
           <div className="row">
             <div className="col-md-2 fb-left-sidebar">
               <LeftSideBar />
