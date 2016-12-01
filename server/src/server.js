564c8e54ac23cf07038229ae88bfac51ff446f12
zarinanorton <norton.zarina@gmail.com>
2016-12-01 12:57:28 -0500

    static

--- server/src/server.js
+++ server/src/server.js
@@ -1,27 +1,14 @@
 // Imports the express Node module.
 var express = require('express');
-var utilModule = require('./util.js');
-var reverseString = utilModule.reverseString;
+//var utilModule = require('./util.js');
+//var reverseString = utilModule.reverseString;
 var bodyParser = require('body-parser');
 // Creates an Express server.
 var app = express();
 app.use(bodyParser.text());
-// Defines what happens when it receives the `GET /` request
-app.get('/', function (req, res) {
-res.send('Hello World!');
-});
+app.use(express.static('../client/build'));
+
 // Starts the server on port 3000!
 app.listen(3000, function () {
 console.log('Example app listening on port 3000!');
-});
-
-// Handle POST /reverse [data]
-app.post('/reverse', function (req, res) {
-  if (typeof(req.body) === 'string') {
-  var reversed = reverseString(req.body);
-  res.send(reversed);
-  } else {
-  // POST did not contain a string. Send an error code back!
-  res.status(400).end()
-  }
 });
